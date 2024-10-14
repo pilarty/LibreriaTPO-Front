986419a9f8d04cf301255d83b5f96f2fb0a9ec83
sofiaLappano <csalemme@uade.edu.ar>
2024-10-14 20:19:02 -0300

    Avance homepage

--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,9 +1,18 @@
+import React from "react";
+import "./Homepage.css";
+import "./App.css";
+import logo from '../assets/logo.png'
+
 const Homepage = () => {
-    return(
-        <>
-        <h2>Homepage</h2>
-        </>
-    )
-}
+    return (
+        <div>
+          <div className="header-2">
+            <img className="image-1-4" src={logo} alt="Logo" />
+            <span className="subtitulo">The Golden Feather</span>
+          </div>
+
+        </div>
+      );
+    };
 
 export default Homepage
