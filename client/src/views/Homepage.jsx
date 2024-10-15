69c9eeb5dd13bff630c9c8f5d8da4d01eb27dc6c
sofiaLappano <csalemme@uade.edu.ar>
2024-10-14 21:04:16 -0300

    Avance homepage2

--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Homepage.css";
-import "./App.css";
 import logo from '../assets/logo.png'
 
 const Homepage = () => {
@@ -9,6 +8,9 @@ const Homepage = () => {
           <div className="header-2">
             <img className="image-1-4" src={logo} alt="Logo" />
             <span className="subtitulo">The Golden Feather</span>
+          </div>
+          <div>
+            <input type="text" />
           </div>
 
         </div>
