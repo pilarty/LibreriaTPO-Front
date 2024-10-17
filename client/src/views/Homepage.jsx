3ecd10cef678c899f81916515edd1e4aa494cd23
sofiaLappano <csalemme@uade.edu.ar>
2024-10-16 21:45:06 -0300

    Actualizacion y conexion componente "MenuDesplegable" 2

--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -9,7 +9,7 @@ import CardLibroList from "../components/CardLibroList";
 import MenuDesplegable from "../components/MenuDesplegable";
 import { Link } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 
 const Homepage = () => {
