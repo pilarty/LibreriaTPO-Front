ca5fd98bb4928f1f373d436bfe54c68076ac2b40
Lola Diaz <diazlola1551@gmail.com>
2024-10-17 21:48:02 -0300

    cors????????????

--- client/src/Registrarse.jsx
+++ client/src/Registrarse.jsx
@@ -36,7 +36,7 @@ const Registrarse = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:8080/api/auth/register', {
+      const response = await fetch('http://localhost:4002/api/v1/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
