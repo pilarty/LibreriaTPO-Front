3fa5f4322a60415a2d13e80f6c166d6acfe44cf0
Lola Diaz <diazlola1551@gmail.com>
2024-10-19 23:13:38 -0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Homepage from './Homepage';
-import LoginPage from './LoginPage';
-import Registrarse from './Registrarse';
-
-const App = () => {
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './Navigation'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './Homepage'
+import ListaLibros from './ListaLibros'
+import Carrito from './Carrito'
+import Compra from './Compra'
+import Favoritos from './Favoritos'
+import LibroSolo from './LibroSolo'
+import Registrarse from './Registrarse'
+import Usuario from './Usuario'
+ 
+const App = ()=>{
+ 
   return (
-    <BrowserRouter>
+    <>
+      <Navigation />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/LoginPage' element={<LoginPage />} />
-        <Route path='/Registrarse' element={<Registrarse />} />
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/Libros' element={<ListaLibros/>}/>
+        <Route path='/Carrito' element={<Carrito/>}/>
+        <Route path='/Compra' element={<Compra/>}/>
+        <Route path='/Favs' element={<Favoritos/>}/>
+        <Route path='/Libro' element={<LibroSolo/>}/>
+        <Route path='/Registrarse' element={<Registrarse/>}/>
+        <Route path='/Usuario' element={<Usuario/>}/>
       </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+   
+    </>
+  )
+}
+ 
+export default App
