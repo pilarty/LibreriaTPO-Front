f9d7f00c039445c066678538478faf6a4274c31f
sofia lappano <sofiaselenelappano@gmail.com>
2024-10-20 11:38:12 -0300

    compra sin back, sigue en procesos

--- client/src/components/FormularioCompra.jsx
+++ client/src/components/FormularioCompra.jsx
@@ -2,6 +2,7 @@
 import "../views/Compra.css";
 import React, { useState } from "react";
 
+
 const FormularioCompra = () => {
     const [formulario, setFormulario] = useState({
         nombre: '',
@@ -16,154 +17,174 @@ const FormularioCompra = () => {
     });
 
     const [giftCardAplicada, setGiftCardAplicada] = useState(false); 
+    const [compraRealizada, setCompraRealizada] = useState(false); // Nuevo estado para el popup
+
     const handleChange = (e) => {
         setFormulario({
             ...formulario,
             [e.target.name]: e.target.value
         });
     };
+
+    const handleRealizarCompra = () => {
+        // Aquí puedes agregar lógica para manejar la compra (validaciones, envío de datos, etc.)
+        setCompraRealizada(true); // Muestra el popup
+    };
+
+    const handleRedirigirHome = () => {
+        window.location.href = "/"; // Redirige a la homepage
+    };
+
     return (
-            <div className='seccion-formulario'>
-                <h1> Formulario de Compra</h1>
+        <div className='seccion-formulario'>
+            <h1>Formulario de Compra</h1>
 
-                <form>
-                    <div className='form-group'>
-                        <label htmlFor="nombre">Nombre</label>
-                        <input
-                            type="text"
-                            name="nombre"
-                            id="nombre"
-                            placeholder="Nombre"
-                            value={formulario.nombre}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="apellido">Apellido</label>
-                        <input
-                            type="text"
-                            name="apellido"
-                            id="apellido"
-                            placeholder="Apellido"
-                            value={formulario.apellido}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="codigoPostal">Código Postal</label>
-                        <input
-                            type="text"
-                            name="codigoPostal"
-                            id="codigoPostal"
-                            placeholder="Código Postal"
-                            value={formulario.codigoPostal}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="direccion">Dirección</label>
-                        <input
-                            type="text"
-                            name="direccion"
-                            id="direccion"
-                            placeholder="Dirección"
-                            value={formulario.direccion}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div  className='form-group'>
-                        <label htmlFor="piso">Piso/Departamento</label>
-                        <input
-                            type="text"
-                            name="piso"
-                            id="piso"
-                            placeholder="Piso/Departamento"
-                            value={formulario.piso}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div  className='form-group'>
-                        <label htmlFor="giftCard">Gift Card</label>
-                        <input
-                            type="text"
-                            id="giftCard"
-                            placeholder="Gift Card"
-                            value={formulario.gifcard}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className='form-group'>
-                        <label htmlFor="medioPago">Medio de Pago</label>
-                        <select
-                            name="medioPago"
-                            id="medioPago"
-                            value={formulario.medioPago}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Seleccione un medio de pago</option>
-                            <option value="tarjetaCredito">Tarjeta de Crédito</option>
-                            <option value="tarjetaDebito">Tarjeta de Débito</option>
-                            <option value="mercadoPago">Mercado Pago</option>
-                        </select>
-                    </div>
-
-        {(formulario.medioPago === 'tarjetaCredito' || formulario.medioPago === 'tarjetaDebito') && (
-            <>
+            <form>
                 <div className='form-group'>
-                    <label htmlFor="numeroTarjeta">Número de Tarjeta</label>
+                    <label htmlFor="nombre">Nombre</label>
                     <input
                         type="text"
-                        name="numeroTarjeta"
-                        id="numeroTarjeta"
-                        placeholder="Número de Tarjeta"
-                        value={formulario.numeroTarjeta}
+                        name="nombre"
+                        id="nombre"
+                        placeholder="Nombre"
+                        value={formulario.nombre}
                         onChange={handleChange}
                         required
                     />
                 </div>
-                <div  className='form-group'>
-                    <label htmlFor="numeroSeguridad">Número de Seguridad</label>
+                <div className='form-group'>
+                    <label htmlFor="apellido">Apellido</label>
                     <input
                         type="text"
-                        name="numeroSeguridad"
-                        id="numeroSeguridad"
-                        placeholder="Número de Seguridad"
-                        value={formulario.numeroSeguridad}
+                        name="apellido"
+                        id="apellido"
+                        placeholder="Apellido"
+                        value={formulario.apellido}
                         onChange={handleChange}
                         required
                     />
                 </div>
-                <div  className='form-group'>
-                    <label htmlFor="fechaVencimiento">Fecha de Vencimiento</label>
+                <div className='form-group'>
+                    <label htmlFor="codigoPostal">Código Postal</label>
                     <input
                         type="text"
-                        name="fechaVencimiento"
-                        id="fechaVencimiento"
-                        placeholder="MM/AA"
-                        value={formulario.fechaVencimiento}
+                        name="codigoPostal"
+                        id="codigoPostal"
+                        placeholder="Código Postal"
+                        value={formulario.codigoPostal}
                         onChange={handleChange}
                         required
                     />
                 </div>
+                <div className='form-group'>
+                    <label htmlFor="direccion">Dirección</label>
+                    <input
+                        type="text"
+                        name="direccion"
+                        id="direccion"
+                        placeholder="Dirección"
+                        value={formulario.direccion}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="piso">Piso/Departamento</label>
+                    <input
+                        type="text"
+                        name="piso"
+                        id="piso"
+                        placeholder="Piso/Departamento"
+                        value={formulario.piso}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="giftCard">Gift Card</label>
+                    <input
+                        type="text"
+                        id="giftCard"
+                        placeholder="Gift Card"
+                        value={formulario.gifcard}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="medioPago">Medio de Pago</label>
+                    <select
+                        name="medioPago"
+                        id="medioPago"
+                        value={formulario.medioPago}
+                        onChange={handleChange}
+                        required
+                    >
+                        <option value="">Seleccione un medio de pago</option>
+                        <option value="tarjetaCredito">Tarjeta de Crédito</option>
+                        <option value="tarjetaDebito">Tarjeta de Débito</option>
+                        <option value="mercadoPago">Mercado Pago</option>
+                    </select>
+                </div>
+
+                {(formulario.medioPago === 'tarjetaCredito' || formulario.medioPago === 'tarjetaDebito') && (
+                    <>
+                        <div className='form-group'>
+                            <label htmlFor="numeroTarjeta">Número de Tarjeta</label>
+                            <input
+                                type="text"
+                                name="numeroTarjeta"
+                                id="numeroTarjeta"
+                                placeholder="Número de Tarjeta"
+                                value={formulario.numeroTarjeta}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="numeroSeguridad">Número de Seguridad</label>
+                            <input
+                                type="text"
+                                name="numeroSeguridad"
+                                id="numeroSeguridad"
+                                placeholder="Número de Seguridad"
+                                value={formulario.numeroSeguridad}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="fechaVencimiento">Fecha de Vencimiento</label>
+                            <input
+                                type="text"
+                                name="fechaVencimiento"
+                                id="fechaVencimiento"
+                                placeholder="MM/AA"
+                                value={formulario.fechaVencimiento}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
                     </>
                 )}
             </form>
 
             <div className="boton-comprar">
-                <button onClick={() => {/*funcion para manejar compra*/}}>
+                <button onClick={handleRealizarCompra}>
                     Realizar Compra
                 </button>
             </div>
-        
-         </div>
-);
 
-}
+            {compraRealizada && (
+                <div className="popup-compra">
+                    <div className="popup-contenido">
+                        <h2>¡Tu compra fue realizada con éxito!</h2>
+                        <button onClick={handleRedirigirHome}>Volver a la homepage</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default FormularioCompra;
+
 
--- client/src/components/ListaLibrosCompra.jsx
+++ client/src/components/ListaLibrosCompra.jsx
@@ -2,8 +2,8 @@ import "../views/Compra.css";
 
 const ListaLibros = ({ libros }) => (
     <ul className='lista-libros'>
-        {libros.map((libro, index) => (
-            <li key={index}>
+        {libros.map((libro, index) => ( //recorre cada elemento del array libros y devuelve un nuevo array con los elementos renderizados como JSX.
+            <li key={index}> {/*Es necesario en React cuando se renderizan listas*/}
                 <span>{libro.nombre}</span>
                 <span>{libro.precio}</span>
             </li>
