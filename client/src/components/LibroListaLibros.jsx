7a94c831a5f24e57453707812f9cb351ebd14adc
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2024-10-21 18:32:57 -0300

    boton carrito en ListaLibros arreglado

--- client/src/components/LibroListaLibros.jsx
+++ client/src/components/LibroListaLibros.jsx
@@ -6,10 +6,10 @@ import { useNavigate } from 'react-router-dom';
 const LibroListaLibros = ({ isbn, titulo, autor, precio, sinopsis, image }) => {
     const navigate = useNavigate();
     const imageSrc = image ? `data:image/jpeg;base64,${image}` : 'default-image-path.jpg';
-    
+
     const emailUsuario = "pgarcia@uade.edu.ar";
     // const emailUsuario = sessionStorage.getItem('userEmail'); // DESCOMENTARLO
-    /*
+
     const manejarAgregarACarrito = (isbn) => {
         if (!emailUsuario) {
             navigate('/LoginPage'); 
@@ -19,92 +19,31 @@ const LibroListaLibros = ({ isbn, titulo, autor, precio, sinopsis, image }) => {
                 isbn: isbn, 
                 carrito_mail: emailUsuario
             };
-
-            const URL_PRODUCTOS = `http://localhost:4002/productosCarrito/${emailUsuario}/listaDeProductosCarritoByMail`;
-
-            fetch(URL_PRODUCTOS)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error("Network response was not ok");
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(isbn);
-                    // Verifica si el ISBN ya está en la lista de productos (sin asumir estructuras anidadas)
-                    const libroExistente = data.find(producto_carrito => producto_carrito.libro.isbn === isbn);
-                    if (libroExistente) {
-                        const nuevaCantidad = libroExistente.cantidad + 1; // Aumentar la cantidad en 1
-                        actualizarCantidadEnServidor(nuevaCantidad, isbn);
-                    } else {
-                        agregarProductoACarrito(requestBody);
-                    }
-                })
-                .catch(error => {
-                    console.error("Error al obtener productos del carrito:", error);
-                });
-        }
-    };
-
-    const agregarProductoACarrito = (requestBody) => {
-        fetch(`http://localhost:4002/productosCarrito`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(requestBody)
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json(); 
-        })
-        .then(data => {
-            console.log("Success:", data);
-            alert("Producto agregado al carrito");
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            alert("No se pudo agregar el producto al carrito. Intente de nuevo.");
-        });
-    };
-
-    const actualizarCantidadEnServidor = async (nuevaCantidad, isbn) => {
-        const url = `http://localhost:4002/productosCarrito/ActualizarCantLibro`;
-        const data = {
-            cantidad: nuevaCantidad,
-            isbn: isbn,
-            carrito_mail: emailUsuario,
-        };
     
-        try {
-            console.log('Actualizando cantidad:', nuevaCantidad, 'ISBN:', isbn, 'Email:', emailUsuario);
-    
-            const response = await fetch(url, {
-                method: 'PUT',
+            fetch(`http://localhost:4002/productosCarrito`, {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json"
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify(requestBody)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("No se pudo agregar el producto al carrito");
+                }
+                return response.json(); 
+            })
+            .then(data => {
+                console.log("Producto agregado al carrito:", data);
+                alert("Producto agregado al carrito");
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                alert("No se pudo agregar el producto al carrito. Intente de nuevo.");
             });
-    
-            if (!response.ok) {
-                const errorMessage = await response.text();
-                throw new Error(`Error al actualizar la cantidad: ${response.status} ${errorMessage}`);
-            }
-    
-            const responseData = await response.json();
-            console.log('Cantidad actualizada:', responseData);
-            alert("Cantidad actualizada en el carrito");
-            
-        } catch (error) {
-            console.error('Error:', error);
-            alert("No se pudo actualizar la cantidad. Intente de nuevo.");
         }
     };
-    */
-
+    
     return (
         <div className="book-container">
             <img src={imageSrc} alt={titulo} className="book-image" />
@@ -116,7 +55,7 @@ const LibroListaLibros = ({ isbn, titulo, autor, precio, sinopsis, image }) => {
                     </div>
                     <div className="flex items-center">
                         <span className="book-price">${precio}</span>
-                        <button className="cart-button" /*onClick={() => manejarAgregarACarrito(isbn)}*/>
+                        <button className="cart-button" onClick={() => manejarAgregarACarrito(isbn)}>
                             <img className="img-carrito" src={Carrito} alt="Carrito" />
                         </button>
                     </div>
