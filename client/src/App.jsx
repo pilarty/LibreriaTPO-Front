6fb5115e3f4e3f2236c3bb191182412e02fed5d9
Lola Diaz <diazlola1551@gmail.com>
2024-10-21 22:41:28 -0300

    USUARIO con delete y get

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Homepage from './Homepage';
+import Homepage from './HomePage';
 import LoginPage from './LoginPage';
 import Registrarse from './Registrarse';
-
+import Usuario from './Usuario';
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,6 +11,7 @@ const App = () => {
         <Route path='/' element={<Homepage />} />
         <Route path='/LoginPage' element={<LoginPage />} />
         <Route path='/Registrarse' element={<Registrarse />} />
+        <Route path='/Usuario' element={<Usuario />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -12,12 +12,17 @@ const HomePage = () => {
     navigate('/Registrarse'); // Navegar a la página de registro
   };
 
+  const handleEditUserClick = () => {
+    navigate('/Usuario'); // Navegar a la página de editar usuario
+  };
+
   return (
     <div>
       <h1>¡Bienvenido a The Golden Feather!</h1>
       <p>Autenticación exitosa. Esta es una página temporal.</p>
       <button onClick={handleLoginClick}>Ir a Iniciar Sesión</button>
       <button onClick={handleSignupClick}>Ir a Registrarse</button> {/* Botón adicional para registrarse */}
+      <button onClick={handleEditUserClick}>Editar Usuario</button> {/* Botón adicional para editar usuario */}
     </div>
   );
 };
