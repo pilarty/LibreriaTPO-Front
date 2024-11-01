76f30cb979e6c902e35199b9037993f41d97abbe
sofiaLappano <csalemme@uade.edu.ar>
2024-11-01 18:59:02 -0300

    Update store 2

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit"
 import librosReducer from "./librosSlice"
 import usuariosReducer from "./usuariosSlice"
 import generosReducer from "./generosSlice"
+import registerReducer from "./registerSlice"
 
 export const store = configureStore({
     reducer: {
         libros : librosReducer,
         usuarios : usuariosReducer,
         generos : generosReducer,
+        register : registerReducer,
     },
 })
 
