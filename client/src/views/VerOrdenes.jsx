06669671e825409caa2ba4d121aac46532eab3a3
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2024-11-17 00:10:30 -0300

    arreglos en diseño VerOrdenes

--- client/src/views/VerOrdenes.jsx
+++ client/src/views/VerOrdenes.jsx
@@ -3,10 +3,19 @@ import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getOrdenes } from '../Redux/ordenesSlice';
 import LoadingSpinner from '../components/LoadingSpinner';
+import "./App.css";
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png'
+import Usuario from '../assets/Usuario.png'
+import Carrito from '../assets/Carrito.png'
+import Hamburguesa from '../assets/hamburguesa.png'
+import MenuDesplegable from "../components/MenuDesplegable";
 
 const VerOrdenes = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [pageSize] = useState(10);
+  const navigate = useNavigate();
+  const [menuVisible, setMenuVisible] = useState(false);
 
   const dispatch = useDispatch();
   const { items, loading, error } = useSelector((state) => state.ordenes);
@@ -34,11 +43,41 @@ const VerOrdenes = () => {
   const handleLastPage = () => {
     setCurrentPage(items.totalPages - 1);
   };
+
+  const manejarUsuario = () => {
+    navigate("/Usuario");
+  }
+  const manejarCarrito = () => {
+    navigate("/Carrito");
+  }
+  
+  const manejarHamburguesa = () => {
+    setMenuVisible(!menuVisible);
+  }
   
   if (loading || !items.content) return <LoadingSpinner />;
   if (error) return <p>Error al cargar las ordenes: {error}</p>;
 
   return (
+    <div>
+          <div className="header-2">
+            <a href="/" className="boton-inicio">
+              <img className="logo" src={logo} alt="Logo" />
+              <span className="subtitulo">The Golden Feather</span>
+            </a>
+            <button className="boton-hamburguesa" onClick={manejarHamburguesa}>
+              <img className="img-hamburguesa" src={Hamburguesa} alt="Hamburguesa" />
+            </button>
+            <button className="boton-usuario" onClick={manejarUsuario}>
+              <img className="img-usuario" src={Usuario} alt="Usuario" />
+            </button>
+            <button className="boton-carrito" onClick={manejarCarrito}>
+              <img className="img-carrito" src={Carrito} alt="Carrito" />
+            </button>
+          </div>
+    
+          {menuVisible && <MenuDesplegable></MenuDesplegable>}
+
     <div className="VerOrdenes-contenedor">
       <div className="VerOrdenes-cabecera">
         <h1 className="VerOrdenes-titulo">Órdenes de Compra</h1>
@@ -50,8 +89,6 @@ const VerOrdenes = () => {
           <span>Todas ({items.totalElements})</span>
           <span>|</span>
           <span>En Proceso (41)</span>
-          <span>|</span>
-          <span>En Espera (203)</span>
           <span>|</span>
           <span>Completadas (43)</span>
           <span>|</span>
@@ -71,7 +108,7 @@ const VerOrdenes = () => {
         </div>
         <button className="VerOrdenes-botonFiltrar">Filtrar</button>
         
-        <span className="VerOrdenes-totalItems">323 items</span>
+        <span className="VerOrdenes-totalItems">{items.totalElements} Ordenes</span>
         <div className="VerOrdenes-paginacion">
           <button 
             className="VerOrdenes-botonPagina" 
@@ -108,23 +145,18 @@ const VerOrdenes = () => {
       <table className="VerOrdenes-tabla">
         <thead>
           <tr className="VerOrdenes-encabezadoTabla">
-            <th className="VerOrdenes-columna">
-              <input type="checkbox" className="VerOrdenes-checkboxPrincipal" />
-            </th>
+            
             <th className="VerOrdenes-columna">Orden</th>
             <th className="VerOrdenes-columna">Fecha</th>
             <th className="VerOrdenes-columna">Estado</th>
             <th className="VerOrdenes-columna">Dirección de Envío</th>
             <th className="VerOrdenes-columna">Total</th>
-            <th className="VerOrdenes-columna">Acciones</th>
           </tr>
         </thead>
         <tbody>
           {items.content.map((orden) => (
             <tr key={orden.id} className="VerOrdenes-fila">
-              <td className="VerOrdenes-celda">
-                <input type="checkbox" className="VerOrdenes-checkboxOrden" />
-              </td>
+              
               <td className="VerOrdenes-celda">
                 <div className="VerOrdenes-infoOrden">
                   <span className="VerOrdenes-idCliente">
@@ -153,6 +185,7 @@ const VerOrdenes = () => {
           ))}
         </tbody>
       </table>
+    </div>
     </div>
   );
 };
